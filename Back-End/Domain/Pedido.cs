9dffb3128f9448cb95042e647d9d4fd7c7b45ece
EduardoC <caetanotva5252@gmail.com>
2022-05-06 09:20:01 -0300

    First Commit

---
+++ Back-End/Domain/Pedido.cs
@@ -0,0 +1,9 @@
+﻿public class Pedido{
+
+    public string Id {get; set;}
+    public string Valor {get; set;}
+    public string DataPedido {get;  set;}
+    public string FormaPagId {get;  set;}
+    public string ClienteId {get;  set;}
+    
+}
---
+++ Back-End/Domain/Produto.cs
@@ -0,0 +1,11 @@
+public class Produto{
+
+    public string Id {get; set;}
+    public string Nome {get; set;}
+    public string Descricao {get; set;}
+    public string Preco {get; set;}
+    public string Categoria {get; set;}
+    public string Marca {get; set;}
+    public string Url {get; set;}
+
+}
